84754a427c32931bae5a642cab18e568d2be2523
Divya Rao <dsrao0712@gmail.com>
2021-09-04 00:28:42 +0530

    Time: 40 ms (93.31%), Space: 14.2 MB (73.39%) - LeetHub

---
+++ two-sum/two-sum.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n = len(nums)
+        hashmap = {}
+        arr = []
+        
+        for i in range(0,n):
+            if target-nums[i] in hashmap:
+                arr.append(hashmap[target-nums[i]])
+                arr.append(i)
+                return arr
+            
+            else:
+                hashmap[nums[i]] = i

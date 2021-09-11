2b58c19f7dcd1626baf501d592fbeafb8bbb9fe6
Divya Rao <dsrao0712@gmail.com>
2021-09-12 01:54:32 +0530

    Time: 56 ms (41.85%), Space: 13.8 MB (14.34%) - LeetHub

--- next-greater-element-i/next-greater-element-i.py
+++ next-greater-element-i/next-greater-element-i.py
@@ -6,21 +6,43 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        # Get the index of Nums1[i] in nums2[j]  
-        # Check if the 
+        # stack for comparison
         stack = []
+        # dict for storing next greater element for all the elements of 
+        # nums2
+        
         d = {}
         
-        for num in nums2 :
-          while stack and stack[-1] < num:
+        for num in nums2:
+          # Check if the num if greater than the top of the stack 
+          # If yes , we found the next greater element for the num
+          
+          while(stack and stack[-1] < num):
             top = stack.pop()
             d[top] = num
-          
+            
+          # If the stack is empty or if the num is not greater than the 
+          # top of the stack , append the elements in the stack
           if not stack or stack[-1] > num:
             stack.append(num)
             
+        # These are the elements which dont have a next greater ele , 
+        # In this case , just pop out the elements and store it in the 
+        # dict with value -1 
+        
         while(stack):
-          rest = stack.pop()
-          d[rest] = -1
+          rem = stack.pop()
+          d[rem] = -1
+          
+        # Since , nums1 is a subset of nums2 , return the elements from 
+        # dict d , which are present in nums1
         
         return [d[i] for i in nums1]
+          
+        
+        
+        
+        
+        
+  
+          

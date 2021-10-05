dc94647cf4f9f1a0f02e7446198ed01eae9b713f
Divya Rao <dsrao0712@gmail.com>
2021-10-06 01:39:17 +0530

    Time: 23 ms (49.22%), Space: 13.6 MB (33.81%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def isValid(self, x):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        
+        for char in x :
+            if (char in ["(" , "{" , "["]):
+                
+                stack.append(char)
+                
+            else:
+                
+                if not stack:
+                    return False
+                    
+                current_char = stack.pop()
+                
+                if current_char == "(" :
+                    if char != ")":
+                        return False
+                        
+                if current_char == "[" :
+                    if char != "]":
+                        return False
+                        
+                if current_char == "{":
+                    if char != "}":
+                        return False
+                        
+        if stack :
+            return False
+            
+        return True

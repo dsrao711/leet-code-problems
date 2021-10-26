be0af0a4361994c2f66d736f8ed490a4dd47cc83
Divya Rao <dsrao0712@gmail.com>
2021-10-26 18:24:33 +0530

    Time: 144 ms (68.98%), Space: 15.8 MB (18.96%) - LeetHub

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -1,25 +1,33 @@
 class Solution(object):
-    def majorityElement(self, A):
+    def majorityElement(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        occ = {}
-        flag = 0
-        N = len(A)
-        for i in A :
-            if i in occ :
-                occ[i] += 1
+        
+        ans_index = 0
+        counter = 0
+        n = len(nums)
+        
+        for i in range(0 , n):
+            if(nums[i] == nums[ans_index]):
+                counter += 1
             else:
-                occ[i] = 1
-        count = 0
-        for key in occ :
-            if(occ[key] > N/2):
-                flag = 1
-                count = key
-                break
+                counter -= 1
             
-        if (flag == 0):
-            count = -1
+            if(counter == 0):
+                ans_index = i
+                counter = 1
+         
+        freq = 0
+        res = -1
+        
+        for i in range(0 , n):
+            if(nums[i] == nums[ans_index]):
+                freq += 1 
+                
+        if(freq >= (n/2)):
+            res = nums[ans_index]
             
-        return count
+        return res
+            

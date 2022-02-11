49bf9e03d8bfda4e168f6f5f20852ae5bf6cc34b
Divya Rao <dsrao0712@gmail.com>
2022-02-11 15:49:14 +0530

    Time: 96 ms (25.47%), Space: 13.5 MB (46.29%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -13,10 +13,16 @@ class Solution(object):
         :rtype: ListNode
         """
         
-        # 1. Traverse till the end of both ll
+        # 1. Traverse till the either ll1 or ll2 is not null
+        # Create p1 to point to ll1 , p2 -> ll2 , res -> dummy
         # 2. Create a dummy head to store the res
         # 3. Carry variable to store carry
-        #
+        # 4. sum =  node1.val + node2.val  + carry 
+        # 5. carr = sum / 10
+        # 6. res.val = sum % 10
+        # 7. Advance res 
+        # 8. Advance p1 and p2 till not null
+        
         dummy = ListNode()
         head1 = l1
         head2 = l2
@@ -33,7 +39,7 @@ class Solution(object):
                 y = head2.val
             else:
                 y = 0 
-                
+            # calc sum of lsbs nd carry    
             add = x + y + carry
             carry = add/10
             curr.next = ListNode(add % 10)
@@ -44,7 +50,8 @@ class Solution(object):
                 
             if(head2 is not None):
                 head2 = head2.next
-            
+                
+        # Last carry if present any    
         if(carry > 0):
             curr.next = ListNode(carry)
             

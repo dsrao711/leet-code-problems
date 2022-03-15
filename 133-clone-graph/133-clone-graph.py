58f1de72d755ed81992c3f28e48241130576f8d1
Divya Rao <dsrao0712@gmail.com>
2022-03-15 14:52:05 +0530

    Time: 69 ms (24.23%), Space: 13.6 MB (83.48%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.py
@@ -0,0 +1,32 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        
+        if(node == None):
+            return 
+        
+        oldtoNew = {}
+        
+        def dfs(node):
+            if(node in oldtoNew):
+                return oldtoNew[node]
+            copy = Node(node.val)
+            oldtoNew[node] = copy
+            
+            for i in node.neighbors : 
+                copy.neighbors.append(dfs(i))
+            
+            return copy
+        
+        return dfs(node)
